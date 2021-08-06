f8d1550aeb4353a103f42eaf6b7b317a458858ff
Luis Urueta <lgurueta@misena.edu.co>
2021-08-06 10:15:27 -0500

    Avance1

---
+++ src/views/Inicio.vue
@@ -0,0 +1,210 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/fondo-banner-principal.png')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Atención integral de urgencias a víctimas de ataque con agentes químicos
+          
+          .h2.Txtc1 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Atención integral de urgencias a víctimas de ataque con agentes químicos
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto 33110258
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
+                p.banner-principal__datos__item__texto 48 Horas
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en semanas: 
+                p.banner-principal__datos__item__texto 4 Semanas
+              li.mb-3.banner-principal__datos__item.mb-0
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo.mb-0 Modalidad: 
+                p.banner-principal__datos__item__texto.mb-0 virtual
+              
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p En el contexto colombiano la desaparición de personas es una realidad, dicha situación genera hechos victimizantes que afectan la salud mental a nivel social, familiar y cultural. 
+
+    p En este sentido, el manejo integral de las víctimas y de la persona fallecida no identificada e identificada no reclamada, se constituye en un derecho que obliga al Estado y a otras Instancias a la  implementación de medidas adecuadas para la verdad, justicia, reparación y restitución de derechos. 
+
+    .row.justify-content-center
+      .col-lg-9.mb-4-mb-lg-0
+        p Frente a la realidad mencionada surge el presente programa con el propósito de fortalecer la gestión de los cementerios desde su labor y responsabilidad frente a individualización, identidad y custodia de las personas fallecidas no identificadas (PNI) e identificadas no reclamadas (PINR). 
+
+        p Este reto de formación implica la formación de talento humano que garantice desde sus competencias las condiciones del manejo integral de las personas fallecidas no identificadas (PNI) e identificadas no reclamadas (PINR).
+      .col-8.col-lg-3
+        figure
+          img(src='@/assets/curso/img01.png', alt='personas fallecidas no identificadas (PNI)')
+    
+
+    p El programa se ha estructurado a partir de (1) contenido temático relacionado con (2) la normatividad, los elementos necesarios para la (3) inhumación y exhumación, los procedimientos que corresponden y la gestión de la información de procesos con personas fallecidas no identificadas (4) (PNI) e identificadas no reclamadas (5) (PINR) en cementerios.
+
+    p Además, se han diseñado, seleccionado  y planeado un conjunto de recursos educativos y actividades de aprendizaje para ayudarle a desarrollar conocimientos, habilidades y actitudes para culminar con éxito su aprendizaje. 
+
+    p El programa está dirigido  al personal administrativo y operativo de los cementerios y funcionarios públicos con responsabilidad o competencias en la gestión de cementerios.
+
+    p Recuerde que este programa se desarrolla desde la modalidad virtual e implica el uso de tecnología como medio para acceder a conocimiento, habilidades y actitudes. 
+
+    p Tenga presente las siguientes recomendaciones para iniciar y finalizar con éxito experiencia de aprendizaje:
+
+    ul.lista-ul
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Comprenda el propósito y estructura del programa.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Identifique los resultados de aprendizaje y evidencias que se esperaran de usted.
+      li.d-flex  
+        i.fas.fa-angle-right
+        |	Planee y ejecute una agenda de estudio según los tiempos señalados para esta experiencia de aprendizaje.
+    
+    p.mb-0 Bienvenido.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 2. Justificación<br>del programa
+
+    p En el contexto colombiano la desaparición de personas es una realidad, dicha situación genera hechos victimizantes que afectan la salud mental a nivel social, familiar y cultural. El manejo integral de las víctimas y de la persona fallecida no identificada e identificada no reclamada, se constituye en un derecho que obliga al Estado y a otras Instancias como el cementerio,  que es uno de los estamentos que tienen la responsabilidad y hace parte del equipamiento municipal, a la  implementación de medidas adecuadas, diferenciadas, transformadoras y efectivas para la verdad, justicia, reparación, restitución de derechos y garantías de no repetición en todas sus dimensiones, tal como lo plantea la normatividad vigente, por lo cual se implementan acciones de búsqueda, identificación y entrega en condiciones de dignidad de los cuerpos o restos humanos de las personas desaparecidas. 
+
+    p.mb-0 Es por lo anterior que el SENA en articulación con el sector funerario busca el fortalecimiento de la gestión de los cementerios en Colombia a partir de la cualificación del talento humano para garantizar las condiciones del manejo integral de las personas fallecidas no identificadas e identificadas no reclamadas en el marco de las tendencias de la innovación permanentemente, cambios tecnológicos, necesidades del Estado y de los trabajadores, impactando positivamente en la productividad, la competitividad, la equidad y el desarrollo del sector funerario.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias<br>a desarrollar
+
+    ul.lista-ul.mb-0
+      li.d-flex.mb-0
+        i.fas.fa-angle-right
+        |	#[strong 260601017] - Inhumar restos mortales de acuerdo con la normativa vigente y protocolos del servicio.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 4. Perfil<br>de ingreso
+    
+    p El programa de “Gestión de procesos con personas fallecidas no identificadas (PNI) e identificadas no reclamadas (PINR) en cementerios”, está dirigido a:
+
+    ul.lista-ul.mb-0
+      li.d-flex
+        i.fas.fa-angle-right
+        |	Personal administrativo.
+      li.d-flex
+        i.fas.fa-angle-right
+        |	Operativo de los cementerios.
+      li.d-flex.mb-0
+        i.fas.fa-angle-right
+        |	Funcionarios públicos con responsabilidad o competencias en la gestión de cementerios.
+  
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 5. Perfil<br>de egreso
+    
+    p El egresado estará en capacidad de reconocer las condiciones del manejo integral de las personas fallecidas no identificadas e identificadas no reclamadas en el marco de las tendencias de la innovación permanentemente, cambios tecnológicos, necesidades del Estado y de los trabajadores, impactando positivamente en la productividad, la competitividad, la equidad y el desarrollo del sector.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 3. Estrategia<br>metodológica
+
+    p Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el uso de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+
+    p Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    .row.g-4.justify-content-center.mb-0
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong Las TIC
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/04.svg', alt='El instructor - tutor')
+        p.text-center.mb-0
+          strong El trabajo colaborativo
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
+    .titulo__template--a.mb-4
+      span.h4 Créditos
+
+    
+    .creditos
+      div(
+        v-for="(creditoKey, index) of Object.keys(creditosData)"
+        :class="index != Object.keys(creditosData).length -1 ? 'mb-4' : ''" 
+      )
+        .creditos__titulo {{configTitulos[creditoKey]}}
+        table
+          tbody
+            tr(
+              v-for="(item, idx) of creditosData[creditoKey]" 
+              :key="creditoKey+idx"
+            )
+              td.text-bold(colspan='2' v-html="renderText(item.nombre)")
+              td(colspan='2' v-html="renderText(item.cargo)")
+              td(colspan='3' v-html="renderText(item.centro)")
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({
+    configTitulos: {
+      liderEquipo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
+      contenidoInstruccional: 'CONTENIDO INSTRUCCIONAL',
+      desarrolloProducto:
+        'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
+      gestoresRepositorio: 'GESTORES DE REPOSITORIO',
+    },
+  }),
+  computed: {
+    creditosData() {
+      return this.$config.creditos
+    },
+  },
+  methods: {
+    renderText(textObj) {
+      let newText = ''
+      if (Array.isArray(textObj)) {
+        textObj.forEach((texto, index) => {
+          newText += (index ? '<br/>' : '') + texto
+        })
+      } else {
+        newText += textObj
+      }
+      return newText
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.creditos
+  td, th
+    border-color: $color-sistema-c
+</style>
